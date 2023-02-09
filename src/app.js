294fe5a313e3637bb117120f7843b189c4178476
Santiago Caballero <saantyc12.caballero@gmail.com>
2023-02-09 17:57:46 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const cors = require('cors');
 const routes = require('./routes/index.js');
 const dotenv = require('dotenv')
 dotenv.config()
@@ -12,14 +13,26 @@ server.name = 'API';
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
+const whiteList = ['https://demo-front-foodapp.vercel.app/']
+const options = {
+  origin: (origin, callback) => {
+    if(whiteList.includes(origin)){
+      callback(null, true);
+  }else{
+    callback(new Error('no permitido!'));
+  }
+  }
+}
+server.use(cors(options))
 server.use(morgan('dev'));
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://demo-front-foodapp.vercel.app/'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
+
+// server.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'https://demo-front-foodapp.vercel.app/'); // update to match the domain you will make the request from
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//   next();
+// });
 
 server.use('/', routes);
 
